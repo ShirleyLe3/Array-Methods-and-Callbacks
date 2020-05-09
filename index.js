51f6939ed382a75b99be319b6bcc6eb0e5f109a0
ShirleyLe3 <synchrotronflux@yahoo.com>
2020-05-08 20:36:25 -0400

    push

--- index.js
+++ index.js
@@ -15,36 +15,40 @@ console.log(fifaData);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(data) {
 
     const finalsArr = data.filter(obj => obj.Stage === "Final")
     return finalsArr
 
 };
 
+console.log (getFinals(fifaData))
+
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+
+function getYears(years) {
 
     const finalYears = years.map(obj => obj.Year)
     return finalYears
-
 };
 
-getYears();
+console.log(getYears(getFinals(fifaData)));
+
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
+function getWinners(winners) {
 
     const winnersArr = winners.map(obj => {
         return `The winner is YOU`
     })
-    return winnersArr
+    return winners
 
 };
 
-getWinners();
+console.log(getWinners(getFinals(fifaData)));
+
 
 /* Task 6: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
@@ -54,10 +58,11 @@ Parameters:
  */
 
 function getWinnersByYear(/* code here */) {
-
+    const years = callBack2(getFinals)
+   return callback1(getFinals).maps((teamName, index) => `In ${years[index]}, ${teamName} won the world cup!`) 
 };
 
-getWinnersByYear();
+getWinnersByYear(getWinners, getYears);
 
 /* Task 7: Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
 
@@ -66,16 +71,19 @@ Hint: use `.reduce` */
 
 function getCountryWins(data, teamInitials) {
 
-    const populationTotal = [];
-    populationTotal.push(fifaData.reduce(
-    function (sum, current) {return sum + current.population},0));
+    const teamInitials = [];
+    fifaData.reduce(1, Match) = > {
+        Match.stage === "Finals"?
+    }
+   return
+}   
+    getCountryWins(getFinals, USA)
 
 
-console.log(populationTotal);
 
 
-
-
+ // function (sum, current) {return sum + current.population},0));
+// console.log(getFinals, USA);
 };
 
 getCountryWins();
